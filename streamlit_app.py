223bdf48e9398193fccb2e8b5f597da415b4a773
Somil Agrawal <110314189+Somilrepos@users.noreply.github.com>
2023-10-03 23:07:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,7 @@ if file:
     df = pd.read_csv("./class_dict.csv")
     category = df.loc[category_idx,"class"]
 
-st.write("HELLO")
 if category:    
-  st.write(f"Your prediction is {category} with a accuracy of {(pred[0][category_idx]*100):.2f}")
+  st.title(f"Your prediction is {category} with a accuracy of {(pred[0][category_idx]*100):.2f}")
   print(f"Your prediction is {category} with a accuracy of {(pred[0][category_idx]*100):.2f}")
     
